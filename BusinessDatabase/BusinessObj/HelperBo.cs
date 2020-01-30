26d156dcdbf1cc318265f39da694459aa219b283
ngoduyhien <ngoduyhien@gmail.com>
2020-01-30 11:45:17 +0700

    lan cuoi

---
+++ BusinessDatabase/BusinessObj/HelperBo.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessDatabase.BusinessObj
+{
+    public class HelperBo
+    {
+
+        public List<BsonDocument> GetAll(string collectionName)
+        {
+            var conn = new GCloudDbConnection();
+            conn.database.GetCollection<BsonDocument>(collectionName);
+            var collection = conn.database.GetCollection<BsonDocument>(collectionName);
+            var list = collection.Find<BsonDocument>(new BsonDocument()).ToList();
+            return list;
+        }
+
+    }
+}
---
+++ BusinessDatabase/GCloudDbConnection.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessDatabase
+{
+    public class GCloudDbConnection
+    {
+        public IMongoDatabase database;
+        public MongoClient client;
+        public GCloudDbConnection()
+        {
+            if (client == null)
+            {
+                client = new MongoClient(System.Configuration.ConfigurationSettings.AppSettings.Get("HealthCareDb"));
+                database = client.GetDatabase(System.Configuration.ConfigurationSettings.AppSettings.Get("DbName"));
+            }
+        }
+    }
+}
