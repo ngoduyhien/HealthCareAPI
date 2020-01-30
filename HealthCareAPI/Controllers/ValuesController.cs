87cad6f99c1c8c48e14a85eab79650a2ebc25f30
longngo1313 <nguyenvulong2002@gmail.com>
2020-01-30 10:52:09 +0700

    Long first commit

--- HealthCareAPI/Controllers/ValuesController.cs
+++ HealthCareAPI/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace HealthCareAPI.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Çhuc Mung Nam moi 2020", "Nguyen Vu Long" };
         }
 
         // GET api/values/5
