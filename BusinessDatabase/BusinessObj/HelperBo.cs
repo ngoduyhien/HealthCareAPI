527f751e43add64adb7663f9fa7991d00254492e
ngoduyhien <ngoduyhien@gmail.com>
2020-01-30 13:40:07 +0700

    sample

--- BusinessDatabase/BusinessObj/HelperBo.cs
+++ BusinessDatabase/BusinessObj/HelperBo.cs
@@ -10,15 +10,62 @@ namespace BusinessDatabase.BusinessObj
 {
     public class HelperBo
     {
-
+        /// <summary>
+        /// get all items in "collectionName"
+        /// </summary>
+        /// <param name="collectionName"></param>
+        /// <returns></returns>
         public List<BsonDocument> GetAll(string collectionName)
         {
             var conn = new GCloudDbConnection();
-            conn.database.GetCollection<BsonDocument>(collectionName);
             var collection = conn.database.GetCollection<BsonDocument>(collectionName);
             var list = collection.Find<BsonDocument>(new BsonDocument()).ToList();
             return list;
         }
-
+        /// <summary>
+        /// insert "document" to "collectionName"
+        /// </summary>
+        /// <param name="document"></param>
+        /// <param name="collectionName"></param>
+        /// <returns></returns>
+        public bool Insert(BsonDocument document, string collectionName)
+        {
+            var conn = new GCloudDbConnection();
+            var collection = conn.database.GetCollection<BsonDocument>(collectionName);
+            var isOk = false;
+            try
+            {
+                collection.InsertOneAsync(document);
+                isOk = true;
+             
+            }
+            catch (Exception ex)
+            {
+                isOk = false;
+            }
+            return isOk;
+        }
+        /// <summary>
+        /// delete multiple data by "filter"
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="collectionName"></param>
+        /// <returns></returns>
+        public bool Delete(FilterDefinition<BsonDocument> filter,string collectionName)
+        {
+            var conn = new GCloudDbConnection();
+            var collection = conn.database.GetCollection<BsonDocument>(collectionName);
+            var isOk = false;
+            try
+            {
+                collection.DeleteMany(filter);
+                isOk = true;
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return isOk;
+        }
     }
 }
