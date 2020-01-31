a5f2e6ac816f9d5c951673b9160d803006194280
longngo1313 <nguyenvulong2002@gmail.com>
2020-01-31 08:47:13 +0700

    Add FoodDto.cs Modify GetAll API return List FoodDto

--- BusinessDatabase/BusinessObj/HelperBo.cs
+++ BusinessDatabase/BusinessObj/HelperBo.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Bson;
+﻿using BusinessDatabase.DataObject;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -11,13 +13,21 @@ namespace BusinessDatabase.BusinessObj
     public class HelperBo
     {
 
-        public List<BsonDocument> GetAll(string collectionName)
+        public List<FoodDto> GetAll(string collectionName)
         {
             var conn = new GCloudDbConnection();
             conn.database.GetCollection<BsonDocument>(collectionName);
             var collection = conn.database.GetCollection<BsonDocument>(collectionName);
             var list = collection.Find<BsonDocument>(new BsonDocument()).ToList();
-            return list;
+
+            List<FoodDto> objList = objList = new List<FoodDto>();
+            foreach (var item in list)
+            {
+                objList.Add(BsonSerializer.Deserialize<FoodDto>(item));
+            }
+
+
+            return objList;
         }
 
     }
---
+++ BusinessDatabase/DataObject/FoodDto.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessDatabase.DataObject
+{
+    [BsonIgnoreExtraElements] 
+    public class FoodDto
+    {
+
+        [BsonElement("name")]
+        public string _name { get; set; }
+
+        [BsonElement("donvi")]
+        public string _donvi { get; set; }
+
+        [BsonElement("carb")]
+        public string _carb { get; set; }
+
+    }
+}
